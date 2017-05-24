b8b6a23da0b95ffd4b8d2e1ae1622cda61946ddb
porta8080 <adm.porta8080@gmail.com>
2017-05-24 01:38:57 -0300

    Adicionando NetInfo

--- app/components/Browser.js
+++ app/components/Browser.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { WebView, View } from 'react-native';
+import { WebView, View, NetInfo } from 'react-native';
 
 export default class Browser extends Component {
     static navigationOptions = {
@@ -8,6 +8,20 @@ export default class Browser extends Component {
 
     constructor(){
         super();
+
+        this.state = {
+            is_connected: false
+        }
+
+        NetInfo.isConnected.fetch().then(is_connected => {
+            this.setState({is_connected: is_connected});
+        });
+
+        function handleFirstConnectivityChange(is_connected) {
+            this.setState({is_connected: is_connected});
+        }
+
+        NetInfo.isConnected.addEventListener( 'change', handleFirstConnectivityChange );
     }
 
     componentWillMount(){
@@ -16,7 +30,15 @@ export default class Browser extends Component {
 
     render() {
         return (
-            <WebView source={{uri: 'http://localhost/webview'}} />
+            {
+                this.state.is_connected ? (
+                    <WebView source={{uri: 'http://localhost/webview'}} />
+                ) : (
+                    <View>
+                        <Text>Oops, você está conectado</Text>                        
+                    </View>
+                )
+            }
         );
     }
 }
